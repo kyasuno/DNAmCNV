3efc9215b91dcefc39fe0775111bcb0228ff59af
Katsuhito Yasuno <katsuhito.yasuno@yale.edu>
2025-10-07 12:27:16 -0400

    Correctin to pred.sex assignment

--- R/make_pon.R
+++ R/make_pon.R
@@ -71,8 +71,7 @@ make_pon <- function(sdfs, platform=c("HM450", "EPIC", "EPICv2"), cutoff=-3.5,
   # infer sex
   message("Predicting sex of samples")
   # pred.sex <- parallel::mclapply(sdfs, function(sdf) sesame::inferSex(sdf), mc.cores=n.cores) |> unlist()
-  pred.sex <- parallel::mclapply(sdfs, function(sdf) get_sex_info, platform=platform, cutoff=cutoff,
-                                 mc.cores=n.cores)
+  pred.sex <- lapply(sdfs, function(sdf) get_sex_info(sdf, platform=platform, cutoff=cutoff))
   pred.sex <- lapply(pred.sex, function(lst) lst$pred.sex) |> unlist()
 
   # calculate M+U

d96c0a50576f9c8ae511dc89d7aacceba0f312fc
Katsuhito Yasuno <katsuhito.yasuno@yale.edu>
2025-10-16 12:16:31 -0400

    Make dependency on sesame and sesameData explicit.

--- R/get_sex_info.R
+++ R/get_sex_info.R
@@ -1,7 +1,7 @@
 #' get probe data for sex inference
 #'
 #' @param sdf SigDF object.
-#' @platform character. HM450, EPIC, or EPICv2.
+#' @param platform character. HM450, EPIC, or EPICv2.
 #' @param cutoff a cutoff value of log2(medianY/medianX) for sex inference (default: -3.5).
 #' @returns a named list with pred.sex (predicted sex), medianX (median intensity of X probes), and
 #' medianY (median intensity of Y probes).
@@ -17,13 +17,11 @@ get_sex_info <- function(sdf, platform, cutoff=-3.5) {
     pids <- sdf$Probe_ID
   }
 
-  cleanY <- sesameDataGet(paste0(
-    platform,'.probeInfo'))$chrY.clean
-  xLinked <- sesameDataGet(paste0(
-    platform,'.probeInfo'))$chrX.xlinked
+  cleanY <- sesameData::sesameDataGet(paste0(platform,'.probeInfo'))$chrY.clean
+  xLinked <- sesameData::sesameDataGet(paste0(platform,'.probeInfo'))$chrX.xlinked
 
-  medY <- median(totalIntensities(sdf[pids %in% cleanY, ]))
-  medX <- median(totalIntensities(sdf[pids %in% xLinked, ]))
+  medY <- median(sesame::totalIntensities(sdf[pids %in% cleanY, ]))
+  medX <- median(sesame::totalIntensities(sdf[pids %in% xLinked, ]))
   sex <-  if (log2(medY / medX) < cutoff) "FEMALE" else "MALE"
   list(pred.sex=sex, medianX=medX, medianY=medY)
 }
